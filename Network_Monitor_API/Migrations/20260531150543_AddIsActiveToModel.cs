4be64b151a3590ec00da0814c26f3d3e42917560
w0rthless <mrkubakachannel.deus@gmail.com>
2026-06-16 12:12:26 +0300

    Pre final 1.1.1

---
+++ Network_Monitor_API/Migrations/20260531150543_AddIsActiveToModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Network_Monitor_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsActiveToModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Models",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Models");
+        }
+    }
+}
